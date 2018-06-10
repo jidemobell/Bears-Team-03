afb23fda37d3e1ed3330c95da12c1fbbdd8f0fc4
= <=>
2018-06-10 23:20:14 +0100

    Inital app setup. Created server and MongoDB connection

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const passport = require('passport')
+
+// Initialize Server, Port and DB Setup
+const Server = express()
+const Port = process.env.port || 4000
+mongoose.connect(process.env.DB_HOST)
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to DB: ${process.env.DB_HOST}`)
+})
+mongoose.connection.on('error', () => {
+  console.log('Failed to connect to DB')
+})
+
+// Middleware
+Server.use(cors()) // Cross site requests. Basically we can call the API from our react frontend on a different port
+Server.use(bodyParser.json()) // Adds a body to the request so we can send data in json format
+
+
+// Start Server
+Server.listen(Port, (err) => {
+  if(err) {
+    console.log('Server Error', err)
+  }
+  console.log(`Server running on Port: ${Port}`)
+})
