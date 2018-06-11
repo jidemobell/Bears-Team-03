2d998020c749afdaae3bd8f0a98c73d1eeefdfa4
= <=>
2018-06-11 22:36:04 +0100

    Added Switch and Route to App.js ready for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App.js
-      </div>
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
     );
   }
 }
--- client/src/middleware/async.js
+++ client/src/middleware/async.js
@@ -13,5 +13,5 @@ export default ({ dispatch }) => {
       }
       dispatch(newAction)
     })
-  }
+  } 
 }
