9e397d6c08a13d8e580bd5bad563d26fa51355a6
jidemobell <jidd32@gmail.com>
2018-06-18 04:45:05 +0100

    update expense model

--- models/Expense.js
+++ models/Expense.js
@@ -1,12 +1,41 @@
 const mongoose = require('mongoose')
 
-const ExpenseSchema = mongoose.Schema({
-  name: String,
-  amount: Number,
-  frequency: String,
+const ExpenseSchema = new mongoose.Schema({
+  // name: String,
+  // amount: Number,
+  // frequency: String,
+  // color: String,
+  timestamps: {            
+    createdAt: 'date',
+    updatedAt: 'last_edited'
+   },
+  item: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  paidWith: {          //paid with cash or card
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  frequecy: {
+
+  }, 
   color: String,
+  category : [{
+    decription : String,  //etc food,grocery,transport
+    amount : String 
+     }],
   user: { type: mongoose.Schema.ObjectId, ref: "User" }
 })
+
+
+ExpenseSchema.statics.findByUser = function(){
+
+}
 
 const Expense = module.exports = mongoose.model('Expense', ExpenseSchema)
 
